7fd99e33fdeb4a7051933e167c3542ca2cd7fc5c
Cristopher <t0ofEr97@gmail.com>
2021-06-28 23:45:17 -0400

    perfiles creados, cards de publicaciones, y otras cosas que no recuerdo

--- src/core/showImage.js
+++ src/core/showImage.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { API } from '../config'
 
-const ShowImage = ({image, url}) => (
-    
+const ShowImage = ({item, url}) => (
+    <div className = "product-img">
         <img
-            src={`${API}/${url}/imagen/${image._id}`}
-            alt={image.name}
+            src={`${API}/${url}/imagen/${item._id}`}
+            alt={item.name}
             className="mb-3"
             style={{ maxHeight: "100%", maxWidth: "100%", objectFit: "cover", objectPosition: "center center" }}
         />
-    
+    </div>
 )
 
-export default ShowImage;
+export default ShowImage 
