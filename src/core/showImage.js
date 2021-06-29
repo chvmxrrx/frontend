98cdee921f4337be2874b56676277652be2c7680
Cristopher <t0ofEr97@gmail.com>
2021-06-28 23:49:28 -0400

    agregue los perfiles, showAvatar y showAvatarProfile, modifique cards de proyectos

--- src/core/showImage.js
+++ src/core/showImage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { API } from '../config'
 
-const ShowImage = ({item, url}) => (
+const ShowImage = ({image, url}) => (
     <div className = "product-img">
         <img
-            src={`${API}/${url}/imagen/${item._id}`}
-            alt={item.name}
+            src={`${API}/${url}/imagen/${image._id}`}
+            alt={image.name}
             className="mb-3"
             style={{ maxHeight: "100%", maxWidth: "100%", objectFit: "cover", objectPosition: "center center" }}
         />
